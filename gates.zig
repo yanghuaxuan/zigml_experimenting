0573597c506e820d0beca6ed9b38cfeebb8f87f6
hyang <hyang@hyang.xyz>
2023-09-19 22:09:40 -0700

    xd

--- gates.zig
+++ gates.zig
@@ -8,8 +8,18 @@ const print = std.debug.print;
 const exp = std.math.exp;
 const pow = std.math.pow;
 
-fn sigmoid(x: f64) f64 {
+fn sigmoid(x: f32) f32 {
     return exp(x) / (exp(x) + 1.0);
+}
+
+fn invert_int(v: i32) i32 {
+    if (v != 0) return 0;
+    return 1;
+}
+
+fn round(v: f32) i32 {
+    const math = @cImport(@cInclude("math.h"));
+    return @as(i32, @intFromFloat(math.roundf(v)));
 }
 
 fn rand_float() f32 {
@@ -48,47 +58,110 @@ pub fn main() !void {
     print("\n", .{});
 
     // OR Sample
-    const sample = [_][3]f32 {
-        [3]f32{0, 0, 0},
-        [3]f32{0, 1, 1},
-        [3]f32{1, 0, 1},
-        [3]f32{1, 1, 1},
-    };
-    // NAND sample
     // const sample = [_][3]f32 {
     //     [3]f32{0, 0, 0},
     //     [3]f32{0, 1, 1},
     //     [3]f32{1, 0, 1},
     //     [3]f32{1, 1, 1},
     // };
+    // NAND sample
+    // const sample = [_][3]f32 {
+    //     [3]f32{0, 0, 0},
+    //     [3]f32{0, 1, 0},
+    //     [3]f32{1, 0, 0},
+    //     [3]f32{1, 1, 1},
+    // };
+    // XOR sample
+    // Requires two neurons
+    const xor_sample_or = [_][3]f32 {
+        [3]f32{0, 0, 0},
+        [3]f32{0, 1, 1},
+        [3]f32{1, 0, 1},
+        [3]f32{1, 1, 1},
+    };
+    const xor_sample_and = [_][3]f32 {
+        [3]f32{0, 0, 0},
+        [3]f32{0, 1, 0},
+        [3]f32{1, 0, 0},
+        [3]f32{1, 1, 1},
+    };
+    const xor_sample = [_][3]f32 {
+        [3]f32{0, 0, 1},
+        [3]f32{1, 0, 1},
+        [3]f32{0, 1, 1},
+        [3]f32{1, 1, 0},
+    };
 
-    var b = rand_float() * 1.0e-15;
-    const rate = 1e-3;
-    var w1 = rand_float() * 10.0;
-    var w2 = rand_float() * 10.0;
-    const eps = 1e-3;
-    const train = 100000; // This time we _iterate_ through our cost function 4 more times
-    var result: f32 = 0;
-    var runtime_zero: usize = 0;
+    const train = 1_000_000; // This time we _iterate_ through our cost function 4 more times
+    const runtime_zero: usize = 0;
 
-    for (0..train) |_| {
-        var dw1 = (cost(sample[runtime_zero..sample.len], w1 + eps, w2 , b) - cost(sample[runtime_zero..sample.len], w1, w2, b)) / eps;
-        var dw2 = (cost(sample[runtime_zero..sample.len], w1, w2 + eps, b) - cost(sample[runtime_zero..sample.len], w1, w2, b)) / eps;
-        var db = (cost(sample[runtime_zero..sample.len], w1, w2, b + eps) - cost(sample[runtime_zero..sample.len], w1, w2, b)) / eps;
-        w1 -= dw1 * rate;
-        w2 -= dw2 * rate;
-        b -= db * rate;
-        // We see that it's actually getting better!
-        result = cost(&sample, w1, w2, b);
-        // print("ITER {}: cost = {}, w1 = {}, w2 = {}, b = {}\n", .{i, result, w1, w2, b});
-    }
+    // Single neuron training
+    //
+    // var b = rand_float() * 1.0e-15;
+    // const rate = 1e-3;
+    // var w1 = rand_float() * 10.0;
+    // var w2 = rand_float() * 10.0;
+    // const eps = 1e-3;
+    // var result: f32 = 0;
+    // for (0..train) |_| {
+    //     var dw1 = (cost(sample[runtime_zero..sample.len], w1 + eps, w2 , b) - cost(sample[runtime_zero..sample.len], w1, w2, b)) / eps;
+    //     var dw2 = (cost(sample[runtime_zero..sample.len], w1, w2 + eps, b) - cost(sample[runtime_zero..sample.len], w1, w2, b)) / eps;
+    //     var db = (cost(sample[runtime_zero..sample.len], w1, w2, b + eps) - cost(sample[runtime_zero..sample.len], w1, w2, b)) / eps;
+    //     w1 -= dw1 * rate;
+    //     w2 -= dw2 * rate;
+    //     b -= db * rate;
+    //     // We see that it's actually getting better!
+    //     result = cost(&sample, w1, w2, b);
+    //     // print("ITER {}: cost = {}, w1 = {}, w2 = {}, b = {}\n", .{i, result, w1, w2, b});
+    // }
     // print("ITER {}: cost = {}, w1 = {}, w2 = {}, b = {}\n", .{train, result, w1, w2, b});
+
+    const eps = 1e-3;
+    const rate = 1e-3;
+    // Two neuron training (XOR)
+    //
+    // Get weights for AND
+    var a_w1 = rand_float() * 10.0;
+    var a_w2 = rand_float() * 10.0;
+    var a_b = rand_float() * 1.0e-15;
+    for(0..train) |_| {
+        var dw1 = (cost(xor_sample_and[runtime_zero..xor_sample_and.len], a_w1 + eps, a_w2 , a_b) - cost(xor_sample_and[runtime_zero..xor_sample_and.len], a_w1, a_w2, a_b)) / eps;
+        var dw2 = (cost(xor_sample_and[runtime_zero..xor_sample_and.len], a_w1, a_w2 + eps, a_b) - cost(xor_sample_and[runtime_zero..xor_sample_and.len], a_w1, a_w2, a_b)) / eps;
+        var db = (cost(xor_sample_and[runtime_zero..xor_sample_and.len], a_w1, a_w2, a_b + eps) - cost(xor_sample_and[runtime_zero..xor_sample_and.len], a_w1, a_w2, a_b)) / eps;
+        a_w1 -= dw1 * rate;
+        a_w2 -= dw2 * rate;
+        a_b -= db * rate;
+    }
+
+    // Get weights for OR
+    var o_w1 = rand_float() * 10.0;
+    var o_w2 = rand_float() * 10.0;
+    var o_b = rand_float() * 1.0e-15;
+    for(0..train) |_| {
+        var dw1 = (cost(xor_sample_or[runtime_zero..xor_sample_or.len], o_w1 + eps, o_w2 , o_b) - cost(xor_sample_or[runtime_zero..xor_sample_or.len], o_w1, o_w2, o_b)) / eps;
+        var dw2 = (cost(xor_sample_or[runtime_zero..xor_sample_or.len], o_w1, o_w2 + eps, o_b) - cost(xor_sample_or[runtime_zero..xor_sample_or.len], o_w1, o_w2, o_b)) / eps;
+        var db = (cost(xor_sample_or[runtime_zero..xor_sample_or.len], o_w1, o_w2, o_b + eps) - cost(xor_sample_or[runtime_zero..xor_sample_or.len], o_w1, o_w2, o_b)) / eps;
+        o_w1 -= dw1 * rate;
+        o_w2 -= dw2 * rate;
+        o_b -= db * rate;
+    }
 
     print("Testing model!\n", .{});
 
-    
+    // Single neuron sample
     // Should be very close to its expected value!
-    for (sample) |row| {
-        print("x1: {}, x2: {}, y: {}\n", .{@as(i32, @intFromFloat(row[0])), @as(i32, @intFromFloat(row[1])), sigmoid(row[0] * w1 + row[1] * w2 + b)});
+    // for (sample) |row| {
+    //     print("x1: {}, x2: {}, y: {}\n", .{@as(i32, @intFromFloat(row[0])), @as(i32, @intFromFloat(row[1])), sigmoid(row[0] * w1 + row[1] * w2 + b)});
+    // }
+    
+    // Two neuron sample
+    // Should be very close to its expected value!
+    for (xor_sample) |row| {
+        // (x1 | x2) & ~(x1 & x2)
+        const x1x2or = sigmoid(row[0] * o_w1 + row[1] * o_w2 + o_b);
+        const x1x2iand = @as(f32, @floatFromInt(invert_int(round(sigmoid(row[0] * a_w1 + row[1] * a_w2 + a_b)))));
+        print("x1: {}, x2: {}, y: {}\n", .{@as(i32, @intFromFloat(row[0])), 
+                                           @as(i32, @intFromFloat(row[1])), 
+                                           sigmoid(x1x2or * a_w1 + x1x2iand * a_w2 + a_b)});
     }
 }
